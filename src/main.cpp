c92e23625229b3b04fc049e9d39c9445b9056b97
maartenww <jachtschipwong@hotmail.com>
2026-01-18 12:20:54 +0100

    Basics

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,16 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Tilemap");
     window.setFramerateLimit(144);
 
+    // Run the program as long as the window is open
     while (window.isOpen())
     {
+        // check all the window's events that were triggered since the last iteration of the loop
         while (const std::optional event = window.pollEvent())
         {
+            // Close the window
             if (event->is<sf::Event::Closed>())
             {
                 window.close();
